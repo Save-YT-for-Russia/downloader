6b94c2712c5e288082ace44dff713ed5409a187d
Roman Milovsky <romdev@vivaldi.net>
2024-07-30 16:37:44 +0300

    300724-16:40

--- index.mjs
+++ index.mjs
@@ -31,13 +31,13 @@ let isBusy = false;
 /* Это для общения с кожанным
 bot.on(['/start', '/hello'], async (msg) => {
     let message = {};
-	message.userID = msg.from.id;
-	message.firstName = msg.from.first_name;
-	message.lastName = msg.from.last_name;
-	message.userName = msg.from.username; // ник
-	message.messageDate = msg.date;
-	message.messageText = msg.text;
-	message.messageChat = msg.chat.id;
+    message.userID = msg.from.id;
+    message.firstName = msg.from.first_name;
+    message.lastName = msg.from.last_name;
+    message.userName = msg.from.username; // ник
+    message.messageDate = msg.date;
+    message.messageText = msg.text;
+    message.messageChat = msg.chat.id;
 
     const { users } = db.data;
     const userData =  users.find((users) => users.tgId === message.userID);
@@ -61,47 +61,51 @@ bot.on(['/start', '/hello'], async (msg) => {
 })
 */
 emitter.on('upload_data', (id, filename) => {
-    // закачиваем файл на "сервер"
-    const copyCommand = spawn('scp', [`"${filename}"`, "rom@192.168.1.3:/tmp"], { shell: true });
-    copyCommand.stdout.on('data', (data) => {
-        console.log(`stdout ${data}`);
-        copyCommandStdOutData += data;
-    });
-    chchildild.stderr.on('data', (data) => {
-        console.log(`stderr ${data}`);
-        copyCommandStdErrData += data;
-    });
-    // уведомляем о выполненной задаче
-    if (copyCommandStdErrData.length > 0) {
-        // тут тогда перекачать
-        emitter.emit('upload_data', id, filename)
-    } else {
-        bot.sendMessage(`{"id":"${id}", "status":"done"}`)
-    }
+    let copyCommandStdOutData = ''
+    let copyCommandStdErrData = ''
+
+    console.log('...upload...', filename)
+    // // закачиваем файл на "сервер"
+    // const copyCommand = spawn('scp', [`"${filename}"`, "rom@192.168.1.3:/tmp"], { shell: true });
+    // copyCommand.stdout.on('data', (data) => {
+    //     console.log(`stdout ${data}`);
+    //     copyCommandStdOutData += data;
+    // });
+    // copyCommand.stderr.on('data', (data) => {
+    //     console.log(`stderr ${data}`);
+    //     copyCommandStdErrData += data;
+    // });
+    // // уведомляем о выполненной задаче
+    // if (copyCommandStdErrData.length > 0) {
+    //     // тут тогда перекачать
+    //     emitter.emit('upload_data', id, filename)
+    // } else {
+    //     bot.sendMessage(`{"id":"${id}", "status":"done"}`)
+    // }
 })
 
 
-emitter.on('new_link', async (msg) => {
+emitter.on('new_link', async () => {
     if (!isBusy) { // сервис не занят
         if (messages[0]) { // если есть сообщения - работаем
             let stdOutData = ''; // накопленный результат вывода от скачивалки
             let stdErrData = ''; // накопленные ошибки вывода от скачивалки
             isBusy = true;
-            
+
             // парсим
             let data;
             try {
-                data = JSON.parse(msg)
-            } catch( error) {
+                data = JSON.parse(messages[0])
+            } catch (error) {
                 console.log('Error in JSON:', msg, error)
                 messages.shift() // удаляем ошибочное из массива ссылок
-                emitter.emit('new_link', isBusy, messages)
+                emitter.emit('new_link')
                 return
             }
 
             // выкачиваем
             const child = spawn('yt-dlp', [`"${data.url}"`], { shell: true });
-            
+
             child.stdout.on('data', (data) => {
                 console.log(`stdout ${data}`);
                 stdOutData += data;
@@ -112,32 +116,40 @@ emitter.on('new_link', async (msg) => {
             });
             child.on('exit', (exitCode, killSignal) => { // отлавливаем окончание работы
                 if (exitCode === 0) { // всё хорошо
-                    
-                    if (stdOutData.lenth > 0) {
+                    if (stdOutData.length > 0) {
+                        // console.log('===== EOW', stdOutData.length)
                         messages.shift() // удаляем выкаченное из массива ссылок
-                    } else {
-                        emitter.emit('new_link', isBusy, messages)
+                        //     console.log('......... EOW:', messages)
+
+                        // разбираем накопленный stdout чтобы получить имя скаченного файла
+                        const tmp = stdOutData.match(getResultfileNameRegex)
+                        console.log('...', data.id, '\n',tmp[1])
+                        emitter.emit('upload_data', data.id, tmp[1])
+
+                        // try {
+                        // } catch(error) {
+                        //     console.log('!!!!!!!! error', error)
+                        // }
                         isBusy = false // меняем статус
+                        if (messages.length > 0) { // если ещё остались ссылки - вызываем выкачивалку
+                            emitter.emit('new_link')
+                            return
+                        }
+                    } else { // что-то пошло не так. Перезапускаем.
+                        isBusy = false // меняем статус
+                        emitter.emit('new_link')
                         return
                     }
-
-                    isBusy = false // меняем статус
-                    if (messages.length > 0) { // если ещё остались ссылки - вызываем выкачивалку
-                        // здесь специально вызывам до окончания всей работы. Нода асинхронная - может работать ||                     
-                        emitter.emit('new_link', isBusy, messages)
-                    }
-
-                    // разбираем накопленный stdout чтобы получить имя скаченного файла
-                    const tmp = stdOutData.match(getResultfileNameRegex)
-                    emitter.emit('upload_data', (msg.id, tmp[1]))
-                    
                 } else { // какая-то ошибка - перезапускаем
-                    emitter.emit('new_link', isBusy, messages)
+                    isBusy = false // меняем статус
+                    emitter.emit('new_link', msg)//, isBusy, messages)
+                    return
                 }
             })
-            
-            
+
         }
+    } else {
+        // перезапускаем через n минут
     }
 })
 
@@ -147,7 +159,7 @@ bot.on(/.*youtube.com\/.*/, (msg) => {
     // return msg.reply.photo('http://thecatapi.com/api/images/get');
     console.log(msg.text)
     messages.push(msg.text)
-    emitter.emit('new_link', msg.text)
+    emitter.emit('new_link')
 });
 
 bot.start()
